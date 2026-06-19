fbe0627986429717b1366f92bfda774fb977ec02
saran <saransegu23@gmail.com>
2026-06-19 17:03:53 +0530

    configfile modification

--- playwright.config.js
+++ playwright.config.js
@@ -3,49 +3,40 @@ const { devices } = require('@playwright/test');
 
 const config = {
   testDir: './tests',
-  retries :1,
+  retries: 1,
   workers: 3,
-  /* Maximum time one test can run for. */
-  //10-
   timeout: 30 * 1000,
   expect: {
-  
     timeout: 5000
   },
   
   reporter: 'html',
-  projects : [
+  projects: [
     {
-      name : 'safari',
+      name: 'safari',
       use: {
-
-        browserName : 'webkit',
-        headless : true,
-        screenshot : 'off',
-        trace : 'on',//off,on 
+        browserName: 'webkit',
+        headless: true,
+        screenshot: 'off',
+        trace: 'on',
         ...devices['iPhone 11'],    
       }
-
     },
     {
-      name : 'chrome',
+      name: 'chrome',
       use: {
-
-        browserName : 'chromium',
-        headless : false,
-        screenshot : 'on',
+        browserName: 'chromium',
+        // Highlights: true when running in GitHub Actions, false when running locally
+        headless: process.env.CI ? true : false, 
+        screenshot: 'on',
         video: 'retain-on-failure',
-        ignoreHttpsErrors:true,
-        permissions:['geolocation'],
-        
-        trace : 'on',//off,on
-       // ...devices['']
-       viewport : {width:1000,height:1000}
-         }
-
+        ignoreHttpsErrors: true,
+        permissions: ['geolocation'],
+        trace: 'on',
+        viewport: { width: 1000, height: 1000 }
+      }
     }
-    ]
-
+  ]
 };
 
 module.exports = config;
